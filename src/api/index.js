b32b556f35d739a54966afca051e9303e84f931d
Oscar Pallas Isern <miorrowls@gmail.com>
2018-07-09 13:39:27 +0200

    Fix https again

--- src/api/index.js
+++ src/api/index.js
@@ -6,6 +6,7 @@ const URL = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country
 
 export default function getArtists(country){
 	const url = URL.replace(':country', country)
+	console.log(url)
 	return fetch(url)
 		.then(res => res.json())
 		.then(json => json.topartists.artist)
